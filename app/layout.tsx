bd4aa4758c9af602836234d7a8196584f03a3aa2
nithishp <nithishj03@gmail.com>
2024-09-18 02:09:43 +0530

    added mobile Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Travel',
-  description: 'Travel UI/UX App for Camping',
+  title: 'Green Grow Landscaping',
+  description: 'Australian Landscaping Company',
 }
 
 export default function RootLayout({
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,62 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
-import Button from "./Button"
+'use client'
+import { useState } from "react";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to track menu visibility
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle the menu
+  };
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/logo.png" alt="logo" width={74} height={29} />
       </Link>
 
+      {/* Desktop Menu */}
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
             {link.label}
           </Link>
         ))}
       </ul>
 
-     
-
-      <Image 
-        src="menu.svg"
+      {/* Mobile Menu Icon */}
+      <Image
+        src="/menu.svg"
         alt="menu"
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu} // Toggle menu on click
       />
-    </nav>
-  )
-}
 
-export default Navbar
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <ul className="absolute top-16 left-0 w-full bg-white shadow-lg flex flex-col gap-6 p-4 lg:hidden">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="text-gray-900 text-center py-2 "
+              onClick={() => setIsMenuOpen(false)} // Close the menu on link click
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
