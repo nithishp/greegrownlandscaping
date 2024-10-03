f38ec0b2ab6d5927bc78e4a47ac70f8177966364
Nithish <nithishj03@gmail.com>
2024-10-03 14:39:18 +0530

    added the changes mentioned in whatsapp

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,8 +12,8 @@ const Hero = () => {
   }
 
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row" id='hero'>
-      <div className="hero-map" />
+    <section className="max-container padding-container flex flex-col  gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row" id='hero' style={{backgroundImage: "url('/curve-bg.png')"}}>
+      <div className='flex flex-row' />
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <h1 className="bold-52 lg:bold-88">
@@ -25,7 +25,24 @@ const Hero = () => {
           landscapes that inspire.
         </p>
 
+        <div className="mb-3  flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5).fill(1).map((_, index) => (
+              <Image 
+                src="/star.svg"
+                key={index}
+                alt="star"
+                width={24}
+                height={24}
+              />
+            ))}
+          </div>
 
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            2K
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
+          </p>
+        </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
           <Button type="button" title="Get Started" variant="btn_green" handleClick={handleClick}/>
@@ -37,6 +54,10 @@ const Hero = () => {
           />
         </div>
       </div>
+        <div>
+
+        <img src='/hero-bg.svg' className='object-contain w-[80%] h-[80%] hidden xl:flex '  />
+        </div>
 
     
     </section>
